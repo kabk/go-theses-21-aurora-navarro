a4df49d1e04eda300733817b80cd52eb9daaada3
aurora9721 <55090088+aurora9721@users.noreply.github.com>
2021-01-15 19:40:57 +0100

    landing page animation

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
-// function myFunction() {
-//   document.getElementById("landing").style.color = "red";
-// }
-
-// ------ THIS IS TO GET THE DISTANCE FROM THE ACTUAL TOP OF THE ENTIRE PAGE -------
-
-// var getOffsetTop = function (elem) {
-//
-// 	// Set our distance placeholder
-// 	var distance = 0;
-//
-// 	// Loop up the DOM
-// 	if (elem.offsetParent) {
-// 		do {
-// 			distance += elem.offsetTop;
-// 			elem = elem.offsetParent;
-// 		} while (elem);
-// 	}
-//
-// 	// Return our distance
-// 	return distance < 0 ? 0 : distance;
-// };
-//
-// // Example
-// var elem = document.querySelector('#some-element');
-// var offset = getOffsetTop(elem);
-
 // -------- THIS IS THE CODE THAT MAKES THE SVG GRADIENT APPEAR ON CLICK, IN MOUSE COORDINATES -------
+
+// document.onclick = userClicked;
+// function userClicked() {
+// var x = event.clientX;
+// var y = event.clientY;
+// var touch = document.getElementById("spot2");
+// touch.style.display = '';
+// touch.style.position = 'absolute';
+// touch.style.left = x + 'px';
+// touch.style.top = y + 'px';
+
+
 
 document.onclick = userClicked;
 function userClicked() {
-var x = event.clientX;
-var y = event.clientY;
+var x = event.pageX;
+var y = event.pageY;
 var touch = document.getElementById("spot2");
 touch.style.display = '';
 touch.style.position = 'absolute';
 touch.style.left = x + 'px';
 touch.style.top = y + 'px';
+
+
+
+// var elem = document.getElementsById("spot2")[0];
+// var clone = elmnt.cloneNode(true);
+// document.body.appendChild(cln);
+
+// let spot2 = document.getElementById("spot2")
+// let spotclone = spot2.cloneNode(true)
+
+// touch.style.left = xPosition + 'px';
+// touch.style.top = yPosition + 'px';
+
+
+// touch.style.left = xPosition;
+// touch.style.top = yPosition;
+
+// touch.style.top = offset;
 // if (touch.style.display === "none") {
 //   touch.style.display = "absolute";
 // } else {
